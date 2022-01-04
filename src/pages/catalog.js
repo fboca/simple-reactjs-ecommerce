4ec1cb6321af3afb7434f93689c32fcee5e5ddcd
fboca <bordehai123@gmail.com>
2022-01-04 06:26:45 +0000

    -

--- src/pages/catalog.js
+++ src/pages/catalog.js
@@ -23,7 +23,7 @@ class Catalog extends React.Component {
 
     addToCart(title) {
         var local = localStorage.getItem('cart');
-        if(local!=null&&local!=undefined) {
+        if (local != null && local != undefined) {
             //console.log(localStorage.getItem('cart'))
             local = local.push(title);
         }
@@ -34,24 +34,26 @@ class Catalog extends React.Component {
     render() {
         console.log(this.state)
         return (
-            <div className="uk-container" style={{ marginTop: 20 }}>
-                <h3>All products</h3>
-                <div class="uk-child-width-1-4@m grid-container" uk-grid>
-                    {this.state.products.map((product) => (
-                        <div>
-                            <div class="uk-card uk-card-default grid-card">
-                                <div class="uk-card-media-top">
-                                    <img src={product.images[1]} alt="" />
+            <div className="container" style={{ marginTop: 20 }}>
+                <div class="container" style={{marginTop: 10}}>
+                <h3 style={{padding: 25}}>All products</h3>
+                    <div class="row row-cols-3">
+                        {this.state.products.map((product) => (
+                            <div class="col">
+                                <div class="uk-card uk-card-default grid-card">
+                                    <div class="uk-card-media-top">
+                                        <img style={{width: '100%', height: 200, objectFit: 'cover' }} src={product.images[0]} alt="" />
+                                    </div>
+                                    <div class="uk-card-body">
+                                        <h5 class="uk-card-title">{product.title}</h5>
+                                    </div>
+                                    <button onClick={() => this.addToCart(product.title)} className="button-cart">Add to cart</button>
                                 </div>
-                                <div class="uk-card-body">
-                                    <h3 class="uk-card-title">{product.title}</h3>
-                                    <p>{product.description}</p>
-                                </div>
-                                <button onClick={() => this.addToCart(product.title)} className="button-cart">Add to cart</button>
                             </div>
-                        </div>
-                    ))}
+                        ))}
+                    </div>
                 </div>
+
             </div>
         )
     }
